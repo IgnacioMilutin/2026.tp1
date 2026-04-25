25e4f36c9bd9e35a4ea1bad0e9ee88de707ed27e
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2026-04-25 14:10:39 -0300

    feat: add recurso service

---
+++ tp1/src/main/java/com/bibliotech/service/RecursoService.java
@@ -0,0 +1,36 @@
+package com.bibliotech.service;
+
+import com.bibliotech.model.Recurso;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RecursoService {
+    private List<Recurso> recursos;
+
+    //contructor
+    public RecursoService(List<Recurso> recursos) {
+        this.recursos = recursos;
+    }
+
+    //Metodo de busqueda de recursos por titulo, luego autor y luego categoria
+    public List<Recurso> buscarRecursos(String busqueda) throws RecursoNoEncontradoException{
+        String busquedaLower=busqueda.toLowerCase();
+
+        //filtro
+        List<Recurso> resultado = this.recursos.stream()
+                .filter(recurso ->
+                        recurso.titulo().toLowerCase().contains(busquedaLower) ||
+                        recurso.autor().toLowerCase().contains(busquedaLower) ||
+                        recurso.categoria().toLowerCase().contains(busquedaLower)
+                ).collect(Collectors.toList());
+
+        //Si la lista esta vacia lanza excepcion
+        if (resultado.isEmpty()) {
+            throw new RecursoNoEncontradoException(busqueda);
+        }
+
+        return resultado;
+    }
+
+}
