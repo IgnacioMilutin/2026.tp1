68c96febb003f0f69f45cd659b97c7bf8a900d82
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2026-04-26 22:49:10 -0300

    feat: Add Socios service

---
+++ tp1/src/main/java/com/bibliotech/service/SocioService.java
@@ -0,0 +1,42 @@
+package com.bibliotech.service;
+
+import com.bibliotech.model.Socio;
+
+import java.util.List;
+
+public class SocioService {
+    private List<Socio> socios;
+    private int contadorId = 1;
+
+    //constructor
+    public SocioService(List<Socio> socios) {
+        this.socios = socios;
+    }
+
+    //metodo para crear socio con validaciones
+    public void nuevoSocio(String tipo,String dni,String nombre, String email){
+
+        int id = contadorId;
+
+        String tipoLower = tipo.toLowerCase();
+
+        //validacion email
+        if (!email.matches("([a-zA-Z0-9._%-]+)@([a-zA-Z0-9.-]+)\\.([a-zA-Z]{2,6})")) {
+            throw new EmailInvalidoException(email);
+        }
+
+        if (tipoLower.equals("docente")){
+            Docente docente = new Docente(id,dni,nombre,email);
+            socios.add(docente);
+        } else if (tipoLower.equals("estudiante")) {
+            Estudiante estudiante = new Estudiante(id,dni,nombre,email);
+            socios.add(estudiante);
+        } else {
+            throw new TipoSocioInexistenteException(tipo);
+        }
+
+        contadorId+=1;
+
+    }
+
+}
